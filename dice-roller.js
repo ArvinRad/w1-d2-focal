150ab7ac3f164c5a9e890564175d4c4e80628e57
ArvinRad <radanarvin@gmail.com>
2021-03-03 06:56:28 +0000

    Dic- roller

---
+++ dice-roller.js
@@ -0,0 +1,6 @@
+const dice = process.argv;
+let rolles = []; 
+for (let i = 0; i < dice[2] ; i++) {
+  rolles.push(Math.floor(Math.random() * 6) + 1);
+};
+console.log(`Rolled ${dice[2]} dice: ${rolles.join(`, `)}`);
